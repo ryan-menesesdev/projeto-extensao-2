f391477400c4cf3e54ecc418584138b8c0aeda6c
Ryan Meneses <ryanmeneses.dev@gmail.com>
2025-11-02 16:02:49 -0300

    add: new views added

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ app.set('views', './app/views');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware para simular autenticação de usuário
-app.use((req, res, next) => {
-  // Simula um login de SUPERVISOR (Pode ver tudo)
-  req.user = { id: 6, nome: 'Thiago Supervisor Teste', role: 'supervisor' };
-  next(); 
+app.use(express.static('public'));
+
+app.use(router);
+
+app.use((req, res) => {
+    res.status(404).json( { error: 'Rota não encontrada' });
 });
 
 
