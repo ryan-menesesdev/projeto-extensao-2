b76c54ce5a3a03ca4ba64f00ade50011660ce7a2
Ryan Meneses <ryanmeneses.dev@gmail.com>
2025-11-29 02:32:08 -0300

    add: Vercel config added

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql2');
 
-module.exports = () => {
-    return dbConn = mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
-    });
-}
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
+module.exports = () => {
+    return pool;
+}
